7a00f326916233e37c74bf8fa42555e67dba7fda
Danny Byrne <dannybyrne@DANIELs-MacBook-Pro.local>
2020-06-23 12:55:56 -0700

    created server index

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const  { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+
+const PokeAPI = require('./datasources/poke');
+
+/**
+ * ApolloServer allows us to fire up an Apollo server which accepts an object containinng the schema, resolves, and datasources we've defined. 
+ */
+
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
+    PokeAPI: new PokeAPI()
+  })
+});
+
+server.listen().then(({ url }) => {
+  console.log(`GQL server running at ${url}`)
+})
---
+++ server/resolvers.js
@@ -0,0 +1,16 @@
+/**
+ * Resolvers allow us to define the mapping of the queries we've defined in our schema to the data source method which returns the data
+ * follows the format: fieldName: (parent, args, context, info) => data
+ * parent: _  : an object that contains the result of the resilver in the parent type. Always blank, refers to the root of the graph. 
+ * an object containing the arguments passed to the field 
+ * context: this is where cwe can access our data sources.  We can extract the data source from the dataSources property. 
+ * info: an object which contains information about the execution state of the operation.  not necessary in our case, so we leave blank.
+ */
+
+
+module.exports = {
+  Query: {
+    pokemon: (_, { id }, { dataSources }) => 
+      dataSources.pokeAPI.getPokemonById({ id })
+  }
+};
